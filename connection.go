e2a3487e10c1c582d9d58262ae672d36748e6c18
skyforce77 <usulguru@gmail.com>
2018-10-16 17:16:43 +0200

    1.13.2 pre 1

--- connection.go
+++ connection.go
@@ -121,6 +121,7 @@ const (
 	V1_12_2 Protocol = 340
 	V1_13   Protocol = 393
 	V1_13_1 Protocol = 401
+	V1_13_2 Protocol = 402
 )
 
 var (
@@ -134,6 +135,7 @@ var (
 		V1_12_2,
 		V1_13,
 		V1_13_1,
+		V1_13_2,
 	}
 )
 
